46e4cc1d6dd2813020458745c87005ec869b4ebd
EnissayDev <bilogamer123@gmail.com>
2024-04-16 01:03:29 +0100

    Added instructions V: 0.2

--- src/main/java/org/enissay/Main.java
+++ src/main/java/org/enissay/Main.java
@@ -22,22 +22,41 @@ public class Main {
         }
 
         System.out.println(OsuUtils.getImageToSBSize("C:\\Users\\Yassine\\AppData\\Local\\osu!\\Songs\\2062263 Camellia - Parallel Universe Shifter\\Camellia_ParallelUniverseShifter_Jacket_2732x1536.jpg"));
+        //Create storyboard with path directory
         Storyboard sb = new Storyboard("");
+
+        //Create a sprite example in the background
         Sprite sp1 = new Sprite(Layer.BACKGROUND, Origin.CENTRE, "dfsfsdfsddfsfds\\ffsgdfdssfd.png");
+
+        //Create some events
         Command cmd = sp1.createLoop(10000, 30);
         Command fade = sp1.Fade(1000, 20000, 0.5, 1);
         Command mv = sp1.Move(1000, 1000, 100, 300, 234, 432);
         Command vs = sp1.VectorScale(975, 975, 0.2, 0.0003, 0.2, 0.0003);
+
         sp1.Scale(975, 980, 0.2, 0.2);
         sp1.Rotate(980, 980, 0.2, 0.3);
+
+        //Add some events to loop
+        /**
+         * _L,(starttime),(loopcount)
+         * __(event),(easing),(relative_starttime),(relative_endtime),(params...)
+         * More events allowed
+         */
         cmd.addSubCommand(fade);
         cmd.addSubCommand(mv);
         cmd.addSubCommand(vs);
+
+        //Create an animation example in the background that loops forever
         Animation sp2 = new Animation(Layer.BACKGROUND, Origin.CENTRE, "dfsfsdfsddfsfds\\XDXDXD.png", 5, 30, LoopType.LOOP_FOREVER);
+        //Create some events
         sp2.Parameter(1000, 3000, 'H');
-        sp2.Color(1000, 3000, Color.BLUE, Color.BLUE);
+        sp2.Color(1000, 3000, Color.BLUE, Color.GREEN);
+
+        //Add the two objects to the storyboard ofc
         sb.addObject(sp1);
         sb.addObject(sp2);
+
         //SBObject sp2 = new SBObject(Layer.BACKGROUND, Origin.CENTRE, "Camellia_ParallelUniverseShifter_Jacket_2732x1536.jpg", 0, 0);
 
         System.out.println(sb.toString());
